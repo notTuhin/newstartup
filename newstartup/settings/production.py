33ea3fb7cdf8c33751282e30ab864559a7f1c024
unknown <mandaltuhin7@gmail.com>
2021-06-29 00:43:35 +0530

    Second Heroku commit

--- newstartup/settings/production.py
+++ newstartup/settings/production.py
@@ -34,7 +34,7 @@ MANAGERS = ADMINS
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = OS.environ.get('SECRET_KEY','django-insecure-*wkh&r&74+j1bwswif(z73p=!-2zh42*n6t1=qj2&r9)(g40qd')
+SECRET_KEY = 'django-insecure-*wkh&r&74+j1bwswif(z73p=!-2zh42*n6t1=qj2&r9)(g40qd'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -94,10 +94,10 @@ WSGI_APPLICATION = 'newstartup.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 # add this
